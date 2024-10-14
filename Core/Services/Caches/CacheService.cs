560fe2bc52d66297984fc644947c2e44f1086937
mali84211 <mali84211@gmail.com>
2024-10-14 22:02:50 +0300

    Dimo

---
+++ Core/Services/Caches/CacheService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Caches
+{
+    public class CacheService : ICacheService
+    {
+        private readonly IDatabase _database;
+        public CacheService(ICacheService redis) {
+            _database=redis.GetDatabase;
+        }
+        public async Task<string> GetCacheKeyAsync(string Key){
+        var cacheResoinse =await _database.StringGetAsync(Key);
+            if (cacheResoinse.IsNullOrEmpty)return null ;
+            return cacheResoinse.ToString() ;
+        }
+        public async Task SetCacheKeyAsync(string Key, object response, TimeSpan expireTime) 
+        {
+            if (response is null) return;
+
+            var option =new JsonSerializerOption() {PropertyNamePolicy=JsonNamingPolicy.CamelCase};
+            await _database.StringSetAsync(Key, JsonSerializer.Serialize(response,option), expireTime);
+        }
+    }
+}
